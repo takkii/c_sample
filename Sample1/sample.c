622d8e384734223fc11d890aa7da424034ba472c
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-07-26 21:59:41 +0900

    こんにちは、世界を記録。

---
+++ Sample1/sample.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   sample.c
+ * Author: sudok
+ *
+ * Created on 2019/07/26, 21:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    // Hello World.
+    printf("こんにちは、世界!");
+    return (EXIT_SUCCESS);
+}
+
